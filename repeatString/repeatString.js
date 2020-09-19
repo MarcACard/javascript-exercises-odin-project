50a3adad9780a19ef9b8f99ea4f8f86d4f2d2403
Marc Card <marcacard@gmail.com>
2020-09-19 16:49:02 -0700

    Update repeatString.js exercise

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function (inputString, repeatNum) {
+  if (repeatNum < 0) return 'ERROR';
+  let output = '';
+  for (let x = 0; x < repeatNum; x += 1) {
+    output += inputString;
+  }
+  return output;
+};
 
-}
-
-module.exports = repeatString
+module.exports = repeatString;
