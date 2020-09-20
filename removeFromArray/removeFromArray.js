84ca9cf20b9f6554300b9dd835834e52827ab5c0
Marc Card <marcacard@gmail.com>
2020-09-19 20:07:42 -0700

    update removeFromArray exercise.

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...args) {
+  const outputArray = [];
+  inputArray.forEach((value) => {
+    if (args.indexOf(value) === -1) {
+      outputArray.push(value);
+    }
+  });
+  return outputArray;
+};
 
-}
-
-module.exports = removeFromArray
+module.exports = removeFromArray;
