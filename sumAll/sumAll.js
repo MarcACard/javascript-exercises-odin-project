715592589c5de741f2f6118065202459498ccab0
Marc Card <marcacard@gmail.com>
2020-09-25 16:24:33 -0700

    Update sumall js exercise.

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+  // Validatation
+  if (min < 0 || max < 0) return 'ERROR';
+  if (typeof min !== 'number' || typeof max !== 'number') return 'ERROR';
 
-}
+  //
+  if (min > max) {
+    const temp = min;
+    min = max;
+    max = temp;
+  }
+  let sum = 0;
+  for (let counter = min; counter <= max; counter += 1) {
+    sum += counter;
+  }
+  return sum;
+};
 
-module.exports = sumAll
+module.exports = sumAll;
